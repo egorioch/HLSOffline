a916d01428f563d86b9badc7346b8f04ab291612
egorioch <egornesobaka@yandex.ru>
2024-09-11 19:31:31 +0300

    добавил множественный выбор камер

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,23 @@
 <template>
+  <div class="video-bar">
+    <div v-for="(vid, idx) in this.suuidMap">
+      <button class="video-button" @click="changeVideoSource(vid)">
+        {{ idx }}, {{ vid }}
+      </button>
+    </div>
+
+  </div>
   <div>
     <h2 align="center">Play Stream {{ this.suuid }}</h2>
     <div class="container">
       <div class="row">
         <div class="col">
           <video
-            id="livestream"
-            style="width: 600px;"
-            controls
-            autoplay
-            muted
+              style="width: 600px;"
+              ref="livestream"
+              controls
+              autoplay
+              muted
           ></video>
         </div>
       </div>
@@ -23,30 +31,60 @@ import Hls from "hls.js";
 export default {
   data() {
     return {
-      suuid: "H264_AAC",
+      suuid: "south_develop_room",
       port: "8083",
-      suuidMap: ["H264_AAC"],
+      suuidMap: ["north_develop_room", "south_develop_room", "kitchen"],
+      activeVideos: [],
+      videosrc: ""
     };
   },
   mounted() {
-    const video = document.getElementById("livestream");
-    const videoSrc = `http://localhost:8083/play/hls/${this.suuid}/index.m3u8`;
+    this.videosrc = `http://localhost:8083/play/hls/${this.suuid}/index.m3u8`;
+    this.setupVideo();
+  },
+  methods: {
+    setupVideo() {
+      const video = this.$refs.livestream;
+      const videoSrc = this.videosrc;
 
-    if (video.canPlayType("application/vnd.apple.mpegurl")) {
-      video.src = videoSrc;
-    } else if (Hls.isSupported()) {
-      const hls = new Hls({
-        autoStartLoad: true,
-        debug: true,
-        manifestLoadingTimeOut: 20000,
-      });
-      hls.loadSource(videoSrc);
-      hls.attachMedia(video);
-    }
+      if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = videoSrc;
+      } else if (Hls.isSupported()) {
+        if (this.hls) {
+          this.hls.destroy();
+        }
+        this.hls = new Hls({
+          autoStartLoad: true,
+          debug: true,
+          manifestLoadingTimeOut: 20000,
+        });
+        this.hls.loadSource(videoSrc);
+        this.hls.attachMedia(video);
+      } else {
+        console.error('Ваш браузер не поддерживает HLS.');
+      }
+    },
+    changeVideoSource(suuid) {
+      this.suuid = suuid;
+      // `videosrc` обновится автоматически через watcher
+    },
+  },
+  watch: {
+    suuid(newSuuid, oldSuuid) {
+      this.videosrc = `http://localhost:8083/play/hls/${newSuuid}/index.m3u8`;
+    },
+    videosrc(newSrc, oldSrc) {
+      this.setupVideo();
+    },
   },
 };
 </script>
 
 <style scoped>
-/* Добавьте свои стили, если необходимо */
+.video-bar {
+  display: flex;
+  width: 100%;
+  height: 50px;
+  justify-content: center;
+}
 </style>
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module HLSOffline
 
 go 1.15
-
-require (
-	github.com/gin-contrib/cors v1.7.2 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
-)
